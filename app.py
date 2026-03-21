9672795ce95c05d13a2a0683e0f1b26386f70e2d
randomPlayerHere <khushalnirwal@gmail.com>
2026-03-21 15:36:58 +0530

    adding utils

--- app.py
+++ app.py
@@ -278,4 +278,4 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001j, reload=True)
